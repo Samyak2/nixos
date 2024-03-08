4fb7700b833c2bacdfea3f2324867fa93355eb76
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-08 20:46:09 +0530

    nixos gen: 46 current  2024-03-08 20:38:14  23.11.20240301.79baff8   6.1.79                          *

--- nvidia.nix
+++ nvidia.nix
@@ -45,6 +45,6 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
