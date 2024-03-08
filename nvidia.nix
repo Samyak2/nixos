bc0358cd8e0b097a015b8bacb5a25c99f4275af4
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-08 22:40:30 +0530

    nixos gen: 48 current  2024-03-08 22:40:26  23.11.20240301.79baff8   6.1.79                          *

--- nvidia.nix
+++ nvidia.nix
@@ -47,4 +47,27 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  # source: https://github.com/TLATER/dotfiles/blob/f60413d2efe1a603fc260280550bb9de858beedb/nixos-config/hosts/yui/nvidia/default.nix#L72C3-L80C5
+  environment.variables = {
+    # Required to run the correct GBM backend for nvidia GPUs on wayland
+    GBM_BACKEND = "nvidia-drm";
+    # Apparently, without this nouveau may attempt to be used instead
+    # (despite it being blacklisted)
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # Hardware cursors are currently broken on nvidia
+    WLR_NO_HARDWARE_CURSORS = "1";
+
+    # firefox vaapi variables:
+    # Required to use va-api it in Firefox. See
+    # https://github.com/elFarto/nvidia-vaapi-driver/issues/96
+    MOZ_DISABLE_RDD_SANDBOX = "1";
+    # It appears that the normal rendering mode is broken on recent
+    # nvidia drivers:
+    # https://github.com/elFarto/nvidia-vaapi-driver/issues/213#issuecomment-1585584038
+    NVD_BACKEND = "direct";
+    # Required for firefox 98+, see:
+    # https://github.com/elFarto/nvidia-vaapi-driver#firefox
+    EGL_PLATFORM = "wayland";
+  };
 }
