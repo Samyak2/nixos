5ffa21d5c21decb78889dcc84ce867a965ad443c
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-08 11:14:29 +0530

    nixos gen: 42 current  2024-03-08 11:14:25  23.11.20240301.79baff8   6.1.79                          *

--- gpg.nix
+++ gpg.nix
@@ -1,6 +1,10 @@
 {...}: {
   programs.gnupg.agent = {
     enable = true;
+    settings = {
+      default-cache-ttl = 6 * 60 * 60; # 6 hours
+      max-cache-ttl = 6 * 60 * 60; # 6 hours
+    };
   };
   services.pcscd.enable = true;
 }
