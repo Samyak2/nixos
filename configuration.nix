381cdc021da98ef3ceff6123e67492208c24111d
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-09 11:59:16 +0530

    nixos gen: 62 current  2024-03-09 11:57:31  24.05.20240306.9df3e30   6.6.19                          *

--- configuration.nix
+++ configuration.nix
@@ -52,8 +52,10 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
   # Enable CUPS to print documents.
