0d8e65206a79875dc8d650ec745efbddd45b46e8
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-17 20:17:14 +0530

    increase alacritty text size and allow amend flag in rebuild
    15 current  2024-03-17 20:18:43  24.05.20240309.3030f18   6.6.21                          *

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+to_amend="$1"
+
 set -euxo pipefail
 
 git --no-pager diff
@@ -23,7 +25,13 @@ git restore --staged hardware-configuration.nix
 
 git --no-pager diff --staged
 
-git commit -m "nixos gen: $gen_only
+git_commit_extra_args=""
+if [[ "$to_amend" == "amend" ]];
+then
+    git_commit_extra_args="--amend"
+fi
+
+git commit $git_commit_extra_args -m "nixos gen: $gen_only
 
 $gen"
 git commit --amend
