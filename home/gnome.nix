52d2e5ad38659ec2c7580a528b9e5206ecad25ac
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-17 19:10:11 +0530

    nixos gen: 6
    6 current   2024-03-17 19:10:05  24.05.20240309.3030f18   6.6.21                          *

--- home/gnome.nix
+++ home/gnome.nix
@@ -1,22 +1,29 @@
 {...}: {
+  # use `dconf watch /` to track changes in real time
   dconf = {
     enable = true;
     settings = {
+      # dark mode
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
+      # no suspend
       "org/gnome/settings-daemon/plugins/power".sleep-inactive-ac-type = "nothing";
 
       "org/gnome/shell" = {
         disable-user-extensions = false;
-
         enabled-extensions = [
           "drive-menu@gnome-shell-extensions.gcampax.github.com"
-
           "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
-
           "appindicatorsupport@rgcjonas.gmail.com"
         ];
       };
+
+      # disable mouse acceleration
+      "org/gnome/desktop/peripherals/mouse".accel-profile = "flat";
+
+      # basic tiling
+      "org/gnome/mutter".edge-tiling = true;
+      "org/gnome/desktop/interface".enable-hot-corners = false;
     };
   };
 }
