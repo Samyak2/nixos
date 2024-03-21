0909d2c1ed6e6b8f16a5ccde036a75b108094754
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-21 12:20:17 +0530

    move zellij config to kdl file
    23 current  2024-03-21 12:20:11  24.05.20240309.3030f18   6.6.21                          *

--- home/zellij.nix
+++ home/zellij.nix
@@ -1,16 +1,10 @@
 {...}: {
   programs.zellij = {
     enable = true;
+  };
 
-    settings = {
-      simplified_ui = true;
-      pane_frames = false;
-      scroll_buffer_size = 100000;
-      copy_on_select = false;
-
-      session_serialization = true;
-      pane_viewport_serialization = true;
-      scrollback_lines_to_serialize = 1000;
-    };
+  xdg.configFile."zellij" = {
+    source = ../config/zellij;
+    recursive = true;
   };
 }
