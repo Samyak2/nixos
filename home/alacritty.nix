c14e31b20dcbda0066c6b5b7b018b8272f03707c
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-23 20:12:51 +0530

    alacritty: make option_as_alt darwin only
    28 current  2024-03-23 20:12:45  24.05.20240309.3030f18   6.6.21                          *

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -1,19 +1,27 @@
-{pkgs-unstable, ...}: {
+{
+  pkgs-unstable,
+  lib,
+  ...
+}: let
+  inherit (pkgs-unstable.stdenv) isDarwin;
+in {
   programs.alacritty = {
     enable = true;
     package = pkgs-unstable.alacritty;
 
     settings = {
-      window = {
-        # background blur
-        # opacity = 0.7;
-        # blur = true;
+      window =
+        {
+          # background blur
+          # opacity = 0.7;
+          # blur = true;
 
-        decorations = "None";
-        # startup_mode = "Maximized";
-
-        option_as_alt = "Both";
-      };
+          decorations = "None";
+          # startup_mode = "Maximized";
+        }
+        // lib.optionalAttrs isDarwin {
+          option_as_alt = "Both";
+        };
 
       font = {
         size = 16;
