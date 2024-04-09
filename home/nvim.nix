3b2d7da551db31b5daf1dbeb35ea9e5ad5b2c9c1
Samyak S Sarnayak <samyak201@gmail.com>
2024-04-09 15:29:08 +0530

    nvim: fix markdown-preview.nvim installation
    nixos gen: 44 current  2024-04-09 14:18:33  24.05.20240325.57e6b3a   6.6.22                          *

--- home/nvim.nix
+++ home/nvim.nix
@@ -15,24 +15,27 @@
       enable = true;
       defaultEditor = true;
 
-      extraPackages = with pkgs-unstable;
-        [
-          gcc # for installing TS grammars
+      extraPackages = with pkgs-unstable; [
+        gcc # for installing TS grammars
 
-          # language servers
-          efm-langserver # generic server for linters, formatters, etc.
-          nil # nix language server
-          lua-language-server # lua
-          nodePackages.pyright # python
-          nodePackages.typescript-language-server # js/ts
-          # the below provides eslint, html, css, json, md
-          vscode-langservers-extracted
-        ]
-        ++ (lib.optionals config.local.copilot-enabled [
-          nodejs_21
-        ]);
+        # language servers
+        efm-langserver # generic server for linters, formatters, etc.
+        nil # nix language server
+        lua-language-server # lua
+        nodePackages.pyright # python
+        nodePackages.typescript-language-server # js/ts
+        # the below provides eslint, html, css, json, md
+        vscode-langservers-extracted
+
+        # required for iamcco/markdown-preview.nvim and copilot
+        nodejs_21
+      ];
     };
 
     home.file.".config/nvim".source = "${inputs.nvim-config}";
+
+    # iamcco/markdown-preview.nvim wants to build a typescript package on the first run.
+    # we instead include the pre-built node_modules available from nixpkgs.
+    home.file.".local/share/nvim/lazy/markdown-preview.nvim/app/node_modules".source = config.lib.file.mkOutOfStoreSymlink "${pkgs-unstable.vimPlugins.markdown-preview-nvim}/app/node_modules";
   };
 }
